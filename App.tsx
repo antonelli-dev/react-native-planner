d7de794746889604e286c68fd1f6e9a9df204a80
antonelli-dev <antonelli.developer@gmail.com>
2025-06-15 21:03:19 +0200

    afsas

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello, Daily Planner!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
